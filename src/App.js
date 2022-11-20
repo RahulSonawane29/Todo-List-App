263fdad06fef4b459f25568388f5b841215ebb2c
RahulSonawane29 <rahulsonawane2908@gmail.com>
2022-11-20 16:26:19 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AppTudo from "./component/tudoList";
+
+function App() {
+  return (
+    <div className="App">
+      <AppTudo />
+    </div>
+  );
+}
+
+export default App;
